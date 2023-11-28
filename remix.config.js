0e2f62cf8b85a7d5c5a2c6973f11630ff373dd39
irisrumtub <tvburis@gmail.com>
2023-11-28 20:09:20 +0300

    test

--- remix.config.js
+++ remix.config.js
@@ -14,6 +14,6 @@ module.exports = {
   serverConditions: ['worker', process.env.NODE_ENV],
   serverDependenciesToBundle: 'all',
   serverModuleFormat: 'esm',
-  serverPlatform: 'node',
+  serverPlatform: 'neutral',
   serverMinify: process.env.NODE_ENV === 'production',
 };
